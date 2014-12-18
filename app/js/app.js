f5a548e2521c812cb37ab31069e8eb8015ce7ede
ionlyseespots <kburas@four51.com>
2014-12-18 10:47:09 -0600

    add color editor

--- app/js/app.js
+++ app/js/app.js
@@ -1,11 +1,12 @@
 var Simple = {};
 
-Simple.app = angular.module('SimpleApp',['ngRoute', 'FBAngular', 'textAngular', 'ngSanitize', 'ui.bootstrap', 'ui.bootstrap.modal']).
+Simple.app = angular.module('SimpleApp',['ngRoute', 'FBAngular', 'textAngular', 'ngSanitize', 'ui.bootstrap', 'ui.bootstrap.modal', 'colorpicker.module']).
     config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/headerTemplate', {templateUrl: 'partials/header.html', controller: 'themeEditorCtrl'});
         $routeProvider.when('/loginBackground', {templateUrl: 'partials/loginBackground.html', controller: 'themeEditorCtrl'});
         $routeProvider.when('/bodyBackground', {templateUrl: 'partials/bodyBackground.html', controller: 'themeEditorCtrl'});
         $routeProvider.when('/newTemplate', {templateUrl: 'partials/newTemplate.html', controller: 'themeEditorCtrl'});
         $routeProvider.when('/desktopTheme', {templateUrl: 'partials/desktopTheme.html', controller: 'themeEditorCtrl'});
+        $routeProvider.when('/colorEditor', {templateUrl: 'partials/colorEditor.html', controller: 'colorThemeCtrl'});
         $routeProvider.otherwise({redirectTo: '/headerTemplate'});
     }]);
