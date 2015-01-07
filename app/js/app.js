cf73fb8ecb66e7cb0d69fc06792ece37f6370867
ionlyseespots <kburas@four51.com>
2015-01-07 13:59:19 -0600

    yup

--- app/js/app.js
+++ app/js/app.js
@@ -4,9 +4,8 @@ Simple.app = angular.module('SimpleApp',['ngRoute', 'FBAngular', 'textAngular', 
     config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/headerTemplate', {templateUrl: 'partials/header.html', controller: 'themeEditorCtrl'});
         $routeProvider.when('/loginBackground', {templateUrl: 'partials/loginBackground.html', controller: 'themeEditorCtrl'});
-        $routeProvider.when('/bodyBackground', {templateUrl: 'partials/bodyBackground.html', controller: 'themeEditorCtrl'});
         $routeProvider.when('/newTemplate', {templateUrl: 'partials/newTemplate.html', controller: 'themeEditorCtrl'});
         $routeProvider.when('/desktopTheme', {templateUrl: 'partials/desktopTheme.html', controller: 'themeEditorCtrl'});
         $routeProvider.when('/colorEditor', {templateUrl: 'partials/colorEditor.html', controller: 'colorThemeCtrl'});
-        $routeProvider.otherwise({redirectTo: '/headerTemplate'});
+        $routeProvider.otherwise({redirectTo: '/desktopTheme'});
     }]);
