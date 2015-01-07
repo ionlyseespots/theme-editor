615c873c53df9ec435f4032e1b3ce722229932a0
ionlyseespots <kburas@four51.com>
2015-01-07 17:50:49 -0600

    fixing bugs, begin final design choices and start on click to copy/paste execution

--- app/js/directives/nav.js
+++ app/js/directives/nav.js
@@ -1,8 +1,18 @@
-Simple.app.directive('navigation', function() {
-    var obj = {
-        restrict: 'E',
-        templateUrl: 'partials/nav.html',
-        controller: 'colorThemeCtrl'
-    };
-    return obj;
+
+Simple.app.directive('trackActive', function($location) {
+    function link(scope, element, attrs){
+        scope.$watch(function() {
+            return $location.path();
+        }, function(){
+            var links = element.find('a');
+            links.removeClass('active');
+            angular.forEach(links, function(value){
+                var a = angular.element(value);
+                if (a.attr('href') == '#' + $location.path() ){
+                    a.addClass('active');
+                }
+            });
+        });
+    }
+    return {link: link};
 });
